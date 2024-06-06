ef2c7829eb460191b11eb860e30044fece45b368
brenda <bbritob8@gmail.com>
2024-06-06 18:11:14 -0300

    inicio da interface de instituicao

--- src/WomenOpportunities/Domain/Interfaces/IInstituicaoRepository.cs
+++ src/WomenOpportunities/Domain/Interfaces/IInstituicaoRepository.cs
@@ -1,6 +1,12 @@
+using WomenOpportunities.Domain.Entities;
+
 namespace WomenOpportunities.Domain.Interfaces;
 
 public interface IInstituicaoRepository
 {
-    
+    void CriarInstituicao(Instituicao instituicao);
+    void AtualizarInstituicao(Instituicao instituicao);
+    void RemoverInstituicao(Instituicao instituicao);
+    Task<Instituicao> ObterInstituicaoPorId(Guid instituicaoId);
+    Task<IEnumerable<Instituicao>> ObterInstituicoes();
 }
